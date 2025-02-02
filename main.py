67fa212f3c2bcd873c647c2ecce49b5bcc0978f3
Brandon Sanchez <ing.brandon.sanchez@gmail.com>
2025-02-02 08:05:00 -0600

    primeros cambios

---
+++ main.py
@@ -0,0 +1,23 @@
+import os
+
+#ingreso los patrones a borrar y agregar
+patron_a_borrar = input("Ingrese el patron a borrar: ")
+patron_a_agregar = input("Ingrese el patron a agregar: ")
+
+
+#listo los elementps del directorio actual y guardo la ruta
+elementos = os.scandir(os.getcwd())
+directorio = os.getcwd()
+
+
+#recorro los elementos del directorio para encontrar el patron y renombrar el archivo
+for elemento in elementos:
+    if elemento.is_file():
+        
+        vieja_ruta = os.path.join(directorio, elemento.name)
+        nuevo_nombre = elemento.name.replace(patron_a_borrar, patron_a_agregar)
+        nueva_ruta = os.path.join(directorio, nuevo_nombre)
+        os.rename(vieja_ruta, nueva_ruta)
+        print(f"Renombrado {elemento.name} a {nuevo_nombre}")
+
+
